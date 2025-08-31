b696ecad4da4cdbfbba0776398cf06168f0ecc1a
iqbal.indra <iqbal.indra@karmaclub.id>
2025-08-31 09:27:15 +0700

    Initial Commit

---
+++ public-api-service/src/main/java/com/example/_9/public_api_service/PublicApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example._9.public_api_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicApiServiceApplication.class, args);
+	}
+
+}
